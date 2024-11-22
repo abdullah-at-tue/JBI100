870f4a47e9ee35f12b0b5c01e07ba104f2f41224
Abdullah A. <alhariri.abdu.2018@gmail.com>
2024-11-22 01:19:38 +0100

    hotfix menu file

--- main/components/main.py
+++ main/components/main.py
@@ -1,4 +1,5 @@
-from main.components import menu, two_scatter_plots
+from main.components.menu import component as menu
+from main.components.two_scatter_plots import component as two_scatter_plots
 from dash import html
 
 def component():
